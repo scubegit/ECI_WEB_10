6bac3bad6d367facd8eced435ffe59dac131c20f
keshavp <keshav.p17@gmail.com>
2024-09-20 17:24:11 +0530

    IS call

--- src/main/resources/static/js/adminJs/adminLogInJS.js
+++ src/main/resources/static/js/adminJs/adminLogInJS.js
@@ -9,8 +9,8 @@ var PFlag1 = "";
 //var url1="http://192.168.42.149:8080/Eci/";
 
 
-var url = "http://192.168.0.100:8081/Eci/Eci/";
-var url1 = "http://192.168.0.100:8081/Eci/Eci/";
+var url = "http://192.168.29.25:8081/Eci/Eci/";
+var url1 = "http://192.168.29.25:8081/Eci/Eci/";
 
 var app_global_path="E:\\Tomcat 10/webapps";
 var app_global_url="http://proapp.rbbn.com"
