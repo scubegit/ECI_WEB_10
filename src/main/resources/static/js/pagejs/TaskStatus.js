a5426e2ad576a189f1fb725a868751789a300420
keshavp <keshav.p17@gmail.com>
2024-09-30 20:30:44 +0530

    SItask page

--- src/main/resources/static/js/pagejs/TaskStatus.js
+++ src/main/resources/static/js/pagejs/TaskStatus.js
@@ -38,7 +38,7 @@
 						    var res = parseInt(test)-parseInt(1);
 				    		var btn = '<td><input type="button" class="table-input-btn Update12" value="Reopen" >'+
 				    		'<input type="button" class="table-input-btn cust-btn-style custom_style_btn" id="generatePdfAction" value="Create ATP" idval="'+data.id+'"  CustomerName="'+data.CustomerName+'" ProductName="'+data.ProductName+'">'+
-				    		'<a data-auto-download href="../ProApp/GeneratePDF/ATP'+res+'.pdf" class="table-input-btn cust-btn-style custom_style_btn" download>Download ATP </a>'+
+				    		'<a data-auto-download href="../ProApp/GeneratePDF/ATP'+res+'.pdf" class="table-input-btn cust-btn-style custom_style_btn downloadATP" download>Download ATP </a>'+
 				    		'<input type="button" class="table-input-btn cust-btn-style custom_style_btn approveStatusId" id="approveStatusId" value="Approve" instId='+data.id+' cnt = '+i+'> </td>';
 				    	} 
 				    	 else
@@ -550,6 +550,37 @@ $(document).on("click", ".update", function(e) {
 			
 		});
 		
+		$(document).on("click", ".downloadATP", function(e){
+	
+			console.log("--------click on approveStatusId--------");
+				
+					instId = $(this).attr("instId");
+					
+					var dataVal = {
+						"instid": instId,
+						"siId":localStorage.getItem("userId"),
+				    	}
+				
+					console.log("----seekApproval----click on dataVal-------",dataVal);
+				
+				$.ajax({
+					
+					type: 'POST',
+					url: url+"insertAtpDownloadAction",  //from API update data
+					data : JSON.stringify(dataVal),
+					processData: false,
+					contentType: "application/json; charset=utf-8",
+		    
+					success: function(result) {
+		    	
+					console.log("Update--seekApproval result==="+result);
+					
+					getListAA();  
+		
+					}
+				});
+		
+		})
 		
 		$(document).on("click", ".approveStatusId", function(e){
 
